deb0553fb50bf148a79a8ab37060d494dbc1508a
Rahul-M01 <rahul.mahajan2@mail.dcu.ie>
2022-10-11 15:34:29 +0100

    Added checker to avoid duplicate additions in database

--- CRUD.py
+++ CRUD.py
@@ -2,16 +2,22 @@ from sqlalchemy.orm import Session
 
 import keygen, models, schemas
 
+def check_db_url_exists(db: Session, url: schemas.URLBase) -> bool:
+    return db.query(models.URL).filter(models.URL.target_url == url.target_url).first()
+
 def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
-    key = keygen.create_unique_random_key(db)
-    secret_key = f"{key}_{keygen.create_random_key(length=8)}"
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    return db_url
+    if(check_db_url_exists(db, url)):
+        return db.query(models.URL).filter(models.URL.target_url == url.target_url).first()
+    else:
+        key = keygen.create_unique_random_key(db)
+        secret_key = f"{key}_{keygen.create_random_key(length=8)}"
+        db_url = models.URL(
+            target_url=url.target_url, key=key, secret_key=secret_key
+        )
+        db.add(db_url)
+        db.commit()
+        db.refresh(db_url)
+        return db_url
 
 def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
     return (
